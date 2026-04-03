b2e02de7d637b6e16191061878e697f2cb960be4
jiwonll <166104093+jiwonll@users.noreply.github.com>
2026-04-03 17:51:53 +0900

    [week-01] 지원: 12903 가운데 글자 가져오기 풀이 완료

---
+++ yoonjiwon/week-01/12903_가운데글자가져오기.js
@@ -0,0 +1,12 @@
+function solution(s) {
+    const len = s.length;
+    
+    if (len % 2 === 0) {
+        return s[len / 2 - 1] + s[len / 2];
+    } else {
+        return s[Math.floor(len / 2)];
+    }
+}
+
+
+// 풀이- 문자열 길이가 짝수인지 홀수인지 조건문으로 구분하고, 짝수인 경우 가운데 두 문자의 인덱스를 계산해 반환하며, 홀수인 경우 Math.floor를 사용해 가운데 인덱스를 구해 한 글자를 반환한다.
