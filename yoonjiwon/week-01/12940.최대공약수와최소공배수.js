9c1ba9d09e08c2cd3fbd67b64952b99ca610aa97
jiwonll <166104093+jiwonll@users.noreply.github.com>
2026-04-05 16:48:52 +0900

    solve: 0405

---
+++ yoonjiwon/week-01/12940.최대공약수와최소공배수.js
@@ -0,0 +1,17 @@
+function solution(n, m) {
+    let maxim = Math.max(n,m);
+    let mini = Math.min(n,m);
+    while(mini !== 0){
+        let temp = mini;
+        mini = maxim % mini;
+        maxim = temp;
+    }
+    let gcd = maxim;
+    let lcm = (n * m) / gcd;
+
+    return [gcd, lcm];
+}
+
+// 풀이
+// 두 수 중 큰값과 작은값을 구분하여 유클리드 호제법으로 최대공약수를 구하고,
+// 최소공배수는 (n * m) / 최대공약수 공식을 이용해 계산하여 반환한다
