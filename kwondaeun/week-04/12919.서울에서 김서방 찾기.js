ccf66c53c4c8543c9ba68826ab65bd39e9d3ace3
DAEUN KWON <dadax0715@gmail.com>
2026-04-20 20:30:08 +0900

    solve: 0420

---
+++ kwondaeun/week-04/12919.서울에서 김서방 찾기.js
@@ -0,0 +1,15 @@
+function solution(seoul) {
+  let kimIndex = seoul.indexOf("Kim");
+
+  return `김서방은 ${kimIndex}에 있다`;
+}
+
+/**
+ * 문제 이해
+ * 배열 중 Kim의 위치를 찾아 "김서방은 x에 있다"를 반환
+ */
+
+/**
+ * 문제 풀이
+ * indexOf()를 통해 배열 seoul에서 Kim의 위치를 찾아 인덱스로 반환
+ */
